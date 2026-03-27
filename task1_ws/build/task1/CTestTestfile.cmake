127e47fda3887e6197cef60bc1c8eb7408b788e3
Favstina <fk68160@student.uni-lj.si>
2026-03-27 09:56:50 +0100

    deleted, wasnt working

--- task1_ws/build/task1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kappa/task1_ws/src/task1
-# Build directory: /home/kappa/task1_ws/build/task1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
